07292d7f5b7c58a906b9b0455dccc7463a8a9e1d
MrMhmods <52112618+MrMhmods@users.noreply.github.com>
2019-07-03 16:07:50 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    TOKEN: 'NTk1MTg2MzUzMzkwNzQ3NjUz.XRylIw.qMQtqeRkIvPnkrZV2mhvJE0XFE8 ',    
+
+    YT_API_KEY: 'yHWaX3KpMQnA13VomQcOlBY6YAkNljvR', 
+
+    prefix: '$',
+
+    devs: ['526702104849678340']
+
+}
